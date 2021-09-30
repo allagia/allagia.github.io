60fc8975c86b68e8699f6c9b1356930f6b75276c
Alla Panova <alla.mixalvna@gmail.com>
2021-09-30 20:03:33 +0300

    добавляет новый проект

--- js/main.js
+++ js/main.js
@@ -1,67 +1,185 @@
-const toggleBtn = document.querySelector('.header__btn');
-const header = document.querySelector('.header');
-const body = document.querySelector('body');
-const mainNav = document.querySelector('.header__list');
-const links = document.querySelectorAll('header__link');
+/** Порядок подключения JS-модулей */
 
-if(toggleBtn) {
+// import noUiSlider from '../vendor/nouislider.js';
 
-  const hideMenu = function () {
-    body.classList.remove('scroll-lock');
-    header.classList.remove('opened');
-  };
+// запуск ползунка для фильтра Розничная цена
+var slider = document.getElementById('slider');
+var sliderTwoHandles = document.getElementById('slider-two-handels');
+var slider2 = document.getElementById('slider-2');
+var slider3 = document.getElementById('slider-3');
+var slider4 = document.getElementById('slider-4');
 
-  const toggleMenu = function () {
-    body.classList.toggle('scroll-lock');
-    header.classList.toggle('opened');
-  };
-
-
-  const onEscKeyDown = function (evt) {
-    if (evt.key === 'Escape') {
-      hideMenu();
-      document.removeEventListener('keydown', function (e) {
-        onEscKeyDown(e);
-      });
-    }
-  };
-
-  const onWindowChange = function () {
-    hideMenu();
-  };
-
-  document.addEventListener('DOMContentLoaded', onWindowChange);
-
-  let width = window.innerWidth;
-  window.addEventListener('resize', () => {
-    if (window.innerWidth !== width) {
-      hideMenu();
-    }
+if (slider) {
+  noUiSlider.create(slider, {
+    start: [50],
+    connect: 'lower',
+    range: {
+      'min': [1],
+      'max': [100],
+    },
   });
+}
 
-  toggleBtn.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    toggleMenu();
-    document.addEventListener('keydown', function (e) {
-      onEscKeyDown(e);
-    });
-  });
 
-  // mainNav.addEventListener('click', function () {
-  //   hideMenu();
-  // });
-
-  // for (let i = 0; i < mainNav.childNodes.length; i++) {
-  //   mainNav.childNodes[i].addEventListener('click', function (event) {
-  //     event.stopPropagation();
-  //   });
-  // }
-
-  links.forEach(function (link){
-    link.addEventListener('click', hideMenu());
+if (sliderTwoHandles) {
+  noUiSlider.create(sliderTwoHandles, {
+    start: [15, 34],
+    connect: true,
+    range: {
+      'min': [1],
+      'max': [100],
+    },
   });
 }
 
 
 
+if (slider3) {
+  noUiSlider.create(slider3, {
+    start: [15, 34],
+    connect: true,
+    tooltips: [true, true],
+    format: wNumb({
+      decimals: 0,
+    }),
+    range: {
+      'min': [1],
+      'max': [100],
+    },
+  });
+}
+
+
+
+if (slider2) {
+  noUiSlider.create(slider2, {
+    start: [50],
+    connect: 'lower',
+    tooltips: [true],
+    format: wNumb({
+      decimals: 0,
+    }),
+    range: {
+      'min': [1],
+      'max': [100],
+    },
+  });
+}
+
+
+if (slider4) {
+  noUiSlider.create(slider4, {
+    start: [15, 34],
+    connect: true,
+    tooltips: [true, true],
+    format: wNumb({
+      decimals: 0,
+    }),
+    range: {
+      'min': [1],
+      'max': [100],
+    },
+  });
+}
+
+var tabs = document.querySelectorAll('.tabs__item--general');
+var tabsIcon = document.querySelectorAll('.tabs__item--icon');
+var tabsSize = document.querySelectorAll('.tabs__item--size');
+var tabsNav = document.querySelectorAll('.tabs-nav');
+
+
+var addActiveClass = function (array) {
+  if (array) {
+    var removeActiveClass = function () {
+      array.forEach(function (tab) {
+        tab.classList.remove('active');
+      });
+    };
+
+    array.forEach(function (tab) {
+      tab.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        removeActiveClass(array);
+        tab.classList.add('active');
+      });
+    });
+  }
+
+};
+
+tabsNav.forEach(function (item) {
+  var tabsItem = item.querySelectorAll('.tab');
+  addActiveClass(tabsItem);
+});
+
+
+addActiveClass(tabs);
+addActiveClass(tabsIcon);
+addActiveClass(tabsSize);
+
+
+var pages = document.querySelectorAll('.pagination-3__link');
+
+if (pages) {
+  addActiveClass(pages);
+}
+
+const tableWithCheckbox = document.querySelector('.table--with-checkbox');
+const mainInput = tableWithCheckbox.querySelector('input[name^=all');
+// const list = tableWithCheckbox.querySelectorAll('.standards-checkbox__list');
+// const nodes = list.querySelectorAll('input[name^=name');
+function checkAll(obj) {
+  const nodes = tableWithCheckbox.querySelectorAll('input[name^=name');
+
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].onclick = function () {
+      const allChecked = tableWithCheckbox.querySelectorAll('input[name^=name]:checked').length;
+      mainInput.checked = allChecked === nodes.length;
+      mainInput.indeterminate = allChecked > 0 && allChecked < nodes.length;
+    };
+  }
+
+  [].forEach.call(nodes, function (elem) {
+    elem.checked = obj.checked;
+  });
+
+}
+
+mainInput.addEventListener('change', function () {
+  checkAll(mainInput);
+});
+
+// анимация (изменение длинны загрузки) происходит после изменений процента загрузки в разметке
+
+// анимация круговой загрузки
+let percentageEntity = document.querySelector('.percentage');
+let textEntity = document.querySelector('.circle');
+
+let percentage = percentageEntity.textContent;
+
+let x = parseInt(percentage.replace(/[^\d]/g, ''));
+
+let attribute = textEntity.getAttribute('stroke-dasharray');
+
+let attributeValue = attribute.substring(0, 2);
+
+if (x !== attributeValue) {
+  textEntity.setAttribute('stroke-dasharray', x + ', 100');
+}
+
+
+// анимация прямой загрузки
+let percent = document.querySelector('.uploader__percent');
+
+let level = document.querySelector('.uploader__level');
+
+level.style.width = percent.textContent + '%';
+
+window.disableLitepickerStyles = true;
+
+new Litepicker({
+  element: document.getElementById('datepicker'),
+  singleMode: false,
+  position: 'bottom',
+})
 
